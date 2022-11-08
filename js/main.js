100071336a885d946311575376fa55094b651624
Amal Madhu <46180807+amalmadhu06@users.noreply.github.com>
2022-11-08 19:13:24 +0530

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 0) {
-    $(".navbar").css("background", "#212529");
-  } else {
-    $(".navbar").css("background", "transparent");
-  }
-});
